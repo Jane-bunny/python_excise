9a69a99afcadf2338a47440f8154742563fa78db
Jane Zhang <jane.zhang886@gmail.com>
2023-02-02 15:18:53 +0000

    good

---
+++ operator_exercise3.py
@@ -0,0 +1,29 @@
+# Assignment Operator 
+lst = 'Jane'
+print(lst)
+
+# Division
+a = 10
+b = 3
+
+result = a / b
+print(result)
+
+# Modulus
+remainder = 1000 % 400
+print(remainder)
+
+#Power
+p_result = 11111 ** 2
+print(p_result)
+
+# Comparision
+lst=["Mango", "Kiwi", "Melon", "Cherry"]
+answer_1=""
+
+if lst[1] == "strawberry":
+    print("Yes")
+else:
+    print("No")
+
+print(answer_1)
